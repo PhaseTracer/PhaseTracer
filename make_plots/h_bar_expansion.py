9aab08f10d66a173ecf251b0f8430ff554fd2619
Andrew Fowlie <andrew.j.fowlie@qq.com>
2020-09-23 20:12:46 +1200

    vary xi too and tree-level tadpole

--- make_plots/h_bar_expansion.py
+++ make_plots/h_bar_expansion.py
@@ -10,8 +10,13 @@ import numpy as np
 import subprocess
 
 
-def call_pt(lambda_hs, Q):
-    r = subprocess.run(["../bin/h_bar_expansion", str(lambda_hs), str(Q)], stdout=subprocess.PIPE, encoding='utf8')
+def call_pt(lambda_hs, Q, xi, tree_level_tadpoles):
+    r = subprocess.run(["../bin/h_bar_expansion",
+                        str(lambda_hs),
+                        str(Q),
+                        str(xi),
+                        str(int(tree_level_tadpoles))],
+                        stdout=subprocess.PIPE, encoding='utf8')
     TC = np.nan
     gamma = np.nan
 
@@ -25,18 +30,21 @@ def call_pt(lambda_hs, Q):
 
 if __name__ == "__main__":
 
+    fig, ax = plt.subplots(1, 2)
+
     lambda_hs = np.linspace(0.2, 0.4)
     mtop = 173.03
-    lower = np.array([call_pt(l, 0.5 * mtop) for l in lambda_hs])
-    central = np.array([call_pt(l, mtop) for l in lambda_hs])
-    upper = np.array([call_pt(l, 2. * mtop) for l in lambda_hs])
+    xi = 0.
+    tree_level_tadpoles = False
 
-    fig, ax = plt.subplots(1, 2)
+    lower = np.array([call_pt(l, 0.5 * mtop, xi, tree_level_tadpoles) for l in lambda_hs])
+    central = np.array([call_pt(l, mtop, xi, tree_level_tadpoles) for l in lambda_hs])
+    upper = np.array([call_pt(l, 2. * mtop, xi, tree_level_tadpoles) for l in lambda_hs])
+
     for i, a in enumerate(ax):
-        a.plot(lambda_hs, lower[:, i], c="red", ls="--", label="$Q = 1/2 m_t$")
-        a.plot(lambda_hs, central[:, i], c="red", ls="-", label="$Q = m_t$")
-        a.plot(lambda_hs, upper[:, i], c="red", ls=":", label="$Q = 2 m_t$")
-        a.set_xlabel("$\lambda_{hs}$")
+        a.plot(lambda_hs, lower[:, i], color="red", ls="--", label="$Q = 1/2 m_t$")
+        a.plot(lambda_hs, central[:, i], color="red", ls="-", label="$Q = m_t$")
+        a.plot(lambda_hs, upper[:, i], color="red", ls=":", label="$Q = 2 m_t$")
 
     compare = np.loadtxt("compare.txt")
     ax[0].plot(compare[:, 0], compare[:, 1], c="grey", ls="--")
@@ -44,11 +52,13 @@ if __name__ == "__main__":
     ax[1].plot(compare[:, 0], compare[:, 3], c="grey", ls="--")
     ax[1].plot(compare[:, 0], compare[:, 4], c="grey", ls=":")
 
-    ax[0].legend()
+    ax[0].legend(fontsize="small")
     ax[0].set_ylim(0., 180.)
     ax[1].set_ylim(0., 4.)
     ax[0].set_ylabel("$T_C$")
     ax[1].set_ylabel("$\gamma$")
+    for a in ax:
+        a.set_xlabel("$\lambda_{hs}$")
 
     plt.tight_layout()
     plt.savefig('h_bar_expansion.pdf')
