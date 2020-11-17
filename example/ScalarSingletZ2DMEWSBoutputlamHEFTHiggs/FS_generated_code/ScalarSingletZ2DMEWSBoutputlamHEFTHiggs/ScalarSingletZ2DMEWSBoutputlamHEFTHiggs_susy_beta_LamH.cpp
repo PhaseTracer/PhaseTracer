670b371a5638f1b59e0b7c69ee3b0deadcd23e67
Peter Atthron <peter.athron@coepp.org.au>
2020-11-17 15:57:20 +1100

    introduce FS_version to cmake, hoping this will let us add code for different FS versions, though may not work until we separate the building fo each FS model. Also remname model scalar singlet model added to use systematic form, reflecting that this is not the obvious version that I will add shortly.

---
+++ example/ScalarSingletZ2DMEWSBoutputlamHEFTHiggs/FS_generated_code/ScalarSingletZ2DMEWSBoutputlamHEFTHiggs/ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_susy_beta_LamH.cpp
@@ -0,0 +1,169 @@
+// ====================================================================
+// This file is part of FlexibleSUSY.
+//
+// FlexibleSUSY is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// FlexibleSUSY is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with FlexibleSUSY.  If not, see
+// <http://www.gnu.org/licenses/>.
+// ====================================================================
+
+// File generated at Tue 17 Nov 2020 15:32:49
+
+#include "ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_susy_parameters.hpp"
+#include "wrappers.hpp"
+
+namespace flexiblesusy {
+
+#define INPUT(parameter) input.parameter
+#define TRACE_STRUCT susy_traces
+
+/**
+ * Calculates the 1-loop beta function of LamH.
+ *
+ * @return 1-loop beta function
+ */
+double ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_susy_parameters::calc_beta_LamH_1_loop(const Susy_traces& susy_traces) const
+{
+   const double traceYdAdjYd = TRACE_STRUCT.traceYdAdjYd;
+   const double traceYeAdjYe = TRACE_STRUCT.traceYeAdjYe;
+   const double traceYuAdjYu = TRACE_STRUCT.traceYuAdjYu;
+   const double traceYdAdjYdYdAdjYd = TRACE_STRUCT.traceYdAdjYdYdAdjYd;
+   const double traceYeAdjYeYeAdjYe = TRACE_STRUCT.traceYeAdjYeYeAdjYe;
+   const double traceYuAdjYuYuAdjYu = TRACE_STRUCT.traceYuAdjYuYuAdjYu;
+
+
+   double beta_LamH;
+
+   beta_LamH = Re(0.01*oneOver16PiSqr*(1200*LamH*traceYdAdjYd - 1200*
+      traceYdAdjYdYdAdjYd + 400*LamH*traceYeAdjYe - 400*traceYeAdjYeYeAdjYe +
+      1200*LamH*traceYuAdjYu - 1200*traceYuAdjYuYuAdjYu + 27*Quad(g1) + 225*
+      Quad(g2) - 180*LamH*Sqr(g1) - 900*LamH*Sqr(g2) + 90*Sqr(g1)*Sqr(g2) +
+      1200*Sqr(LamH) + 100*Sqr(LamSH)));
+
+
+   return beta_LamH;
+}
+
+/**
+ * Calculates the 2-loop beta function of LamH.
+ *
+ * @return 2-loop beta function
+ */
+double ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_susy_parameters::calc_beta_LamH_2_loop(const Susy_traces& susy_traces) const
+{
+   const double traceYdAdjYd = TRACE_STRUCT.traceYdAdjYd;
+   const double traceYeAdjYe = TRACE_STRUCT.traceYeAdjYe;
+   const double traceYuAdjYu = TRACE_STRUCT.traceYuAdjYu;
+   const double traceYdAdjYdYdAdjYd = TRACE_STRUCT.traceYdAdjYdYdAdjYd;
+   const double traceYdAdjYuYuAdjYd = TRACE_STRUCT.traceYdAdjYuYuAdjYd;
+   const double traceYeAdjYeYeAdjYe = TRACE_STRUCT.traceYeAdjYeYeAdjYe;
+   const double traceYuAdjYuYuAdjYu = TRACE_STRUCT.traceYuAdjYuYuAdjYu;
+   const double traceYdAdjYdYdAdjYdYdAdjYd = TRACE_STRUCT.
+      traceYdAdjYdYdAdjYdYdAdjYd;
+   const double traceYdAdjYdYdAdjYuYuAdjYd = TRACE_STRUCT.
+      traceYdAdjYdYdAdjYuYuAdjYd;
+   const double traceYdAdjYuYuAdjYdYdAdjYd = TRACE_STRUCT.
+      traceYdAdjYuYuAdjYdYdAdjYd;
+   const double traceYdAdjYuYuAdjYuYuAdjYd = TRACE_STRUCT.
+      traceYdAdjYuYuAdjYuYuAdjYd;
+   const double traceYeAdjYeYeAdjYeYeAdjYe = TRACE_STRUCT.
+      traceYeAdjYeYeAdjYeYeAdjYe;
+   const double traceYuAdjYuYuAdjYuYuAdjYu = TRACE_STRUCT.
+      traceYuAdjYuYuAdjYuYuAdjYu;
+
+
+   double beta_LamH;
+
+   beta_LamH = Re(0.001*twoLoop*(-3000*LamH*traceYdAdjYdYdAdjYd + 60000*
+      traceYdAdjYdYdAdjYdYdAdjYd - 24000*traceYdAdjYdYdAdjYuYuAdjYd - 42000*
+      LamH*traceYdAdjYuYuAdjYd + 12000*traceYdAdjYuYuAdjYdYdAdjYd - 12000*
+      traceYdAdjYuYuAdjYuYuAdjYd - 1000*LamH*traceYeAdjYeYeAdjYe + 20000*
+      traceYeAdjYeYeAdjYeYeAdjYe - 3000*LamH*traceYuAdjYuYuAdjYu + 60000*
+      traceYuAdjYuYuAdjYuYuAdjYu - 78000*Cube(LamH) - 4000*Cube(LamSH) - 3411*
+      Power6(g1) + 38125*Power6(g2) + 9435*LamH*Quad(g1) + 900*traceYdAdjYd*
+      Quad(g1) - 4500*traceYeAdjYe*Quad(g1) - 3420*traceYuAdjYu*Quad(g1) - 9125
+      *LamH*Quad(g2) - 4500*traceYdAdjYd*Quad(g2) - 1500*traceYeAdjYe*Quad(g2)
+      - 4500*traceYuAdjYu*Quad(g2) + 2500*LamH*traceYdAdjYd*Sqr(g1) + 1600*
+      traceYdAdjYdYdAdjYd*Sqr(g1) + 7500*LamH*traceYeAdjYe*Sqr(g1) - 4800*
+      traceYeAdjYeYeAdjYe*Sqr(g1) + 8500*LamH*traceYuAdjYu*Sqr(g1) - 3200*
+      traceYuAdjYuYuAdjYu*Sqr(g1) - 7225*Quad(g2)*Sqr(g1) + 22500*LamH*
+      traceYdAdjYd*Sqr(g2) + 7500*LamH*traceYeAdjYe*Sqr(g2) + 22500*LamH*
+      traceYuAdjYu*Sqr(g2) - 8385*Quad(g1)*Sqr(g2) + 5850*LamH*Sqr(g1)*Sqr(g2)
+      + 5400*traceYdAdjYd*Sqr(g1)*Sqr(g2) + 6600*traceYeAdjYe*Sqr(g1)*Sqr(g2) +
+      12600*traceYuAdjYu*Sqr(g1)*Sqr(g2) + 80000*LamH*traceYdAdjYd*Sqr(g3) -
+      64000*traceYdAdjYdYdAdjYd*Sqr(g3) + 80000*LamH*traceYuAdjYu*Sqr(g3) -
+      64000*traceYuAdjYuYuAdjYu*Sqr(g3) - 72000*traceYdAdjYd*Sqr(LamH) - 24000*
+      traceYeAdjYe*Sqr(LamH) - 72000*traceYuAdjYu*Sqr(LamH) + 10800*Sqr(g1)*Sqr
+      (LamH) + 54000*Sqr(g2)*Sqr(LamH) - 5000*LamH*Sqr(LamSH)));
+
+
+   return beta_LamH;
+}
+
+/**
+ * Calculates the 3-loop beta function of LamH.
+ *
+ * @return 3-loop beta function
+ */
+double ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_susy_parameters::calc_beta_LamH_3_loop(const Susy_traces& susy_traces) const
+{
+   DEFINE_PROJECTOR(3,3,3,3)
+
+
+
+   double beta_LamH;
+
+   beta_LamH = 0;
+
+
+   return beta_LamH;
+}
+
+/**
+ * Calculates the 4-loop beta function of LamH.
+ *
+ * @return 4-loop beta function
+ */
+double ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_susy_parameters::calc_beta_LamH_4_loop(const Susy_traces& susy_traces) const
+{
+   DEFINE_PROJECTOR(3,3,3,3)
+
+
+
+   double beta_LamH;
+
+   beta_LamH = 0;
+
+
+   return beta_LamH;
+}
+
+/**
+ * Calculates the 5-loop beta function of LamH.
+ *
+ * @return 5-loop beta function
+ */
+double ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_susy_parameters::calc_beta_LamH_5_loop(const Susy_traces& susy_traces) const
+{
+   DEFINE_PROJECTOR(3,3,3,3)
+
+
+
+   double beta_LamH;
+
+   beta_LamH = 0;
+
+
+   return beta_LamH;
+}
+
+} // namespace flexiblesusy
---
+++ example/ScalarSingletZ2DMEWSBoutputlamHEFTHiggs/FS_generated_code/ScalarSingletZ2DMEWSBoutputlamHEFTHiggs/ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_utilities.hpp
@@ -0,0 +1,167 @@
+// ====================================================================
+// This file is part of FlexibleSUSY.
+//
+// FlexibleSUSY is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// FlexibleSUSY is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with FlexibleSUSY.  If not, see
+// <http://www.gnu.org/licenses/>.
+// ====================================================================
+
+// File generated at Tue 17 Nov 2020 15:32:57
+
+#ifndef ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_UTILITIES_H
+#define ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_UTILITIES_H
+
+#include "ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_mass_eigenstates.hpp"
+#include "ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info.hpp"
+#include "wrappers.hpp"
+
+#include <Eigen/Core>
+#include <array>
+#include <string>
+#include <vector>
+#include <valarray>
+#include <utility>
+
+namespace softsusy {
+class QedQcd;
+}
+
+namespace flexiblesusy {
+
+struct ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_observables;
+class Physical_input;
+
+class ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_parameter_getter {
+private:
+   std::vector<std::string> get_mass_names(const std::string& head = "") const {
+      using namespace ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info;
+      std::vector<std::string> masses;
+      for (int i = 0; i < NUMBER_OF_PARTICLES; i++) {
+         for (int m = 0; m < particle_multiplicities[i]; m++) {
+            masses.push_back(
+               head + "M" + particle_names[i] +
+               (particle_multiplicities[i] == 1 ? "" : "("
+                + std::to_string(static_cast<long long>(m)) + ")"));
+         }
+      }
+
+      masses.shrink_to_fit();
+
+      return masses;
+   }
+
+   std::array<std::string, ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_MIXINGS> get_mixing_names() const {
+      return ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::particle_mixing_names;
+   }
+
+public:
+   /// returns DR-bar parameters
+   Eigen::ArrayXd get_parameters(const ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_mass_eigenstates& model) {
+      return model.get();
+   }
+
+   /// returns names of DR-bar parameters
+   std::array<std::string, ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_PARAMETERS> get_parameter_names() const {
+      return ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::parameter_names;
+   }
+
+   /// returns names of particles
+   std::array<std::string, ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_PARTICLES> get_particle_names() const {
+      return ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::particle_names;
+   }
+
+   /// returns names of DR-bar masses
+   std::vector<std::string> get_DRbar_mass_names() const { return get_mass_names(); }
+
+   /// returns names of pole masses
+   std::vector<std::string> get_pole_mass_names() const { return get_mass_names("Pole"); }
+
+   /// returns names of DR-bar mixing matrices
+   std::array<std::string, ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_MIXINGS> get_DRbar_mixing_names() const {
+      return get_mixing_names();
+   }
+
+   /// returns names of pole mixing matrices
+   std::array<std::string, ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_MIXINGS> get_pole_mixing_names() const {
+      auto mixing_names = get_mixing_names();
+      for (auto& n: mixing_names)
+         n = std::string("Pole") + n;
+      return mixing_names;
+   }
+
+   /// returns names of input parameters
+   std::array<std::string, ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_INPUT_PARAMETERS> get_input_parameter_names() const {
+      return ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::input_parameter_names;
+   }
+
+   /// returns names of input parameters
+   std::array<std::string, ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_EXTRA_PARAMETERS> get_extra_parameter_names() const {
+      return ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::extra_parameter_names;
+   }
+
+   /// returns number of particle masses
+   decltype(ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_MASSES) get_number_of_masses() const {
+      return ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_info::NUMBER_OF_MASSES;
+   }
+};
+
+class ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_spectrum_plotter {
+public:
+   ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_spectrum_plotter() = default;
+   explicit ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_spectrum_plotter(const ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_mass_eigenstates&);
+   void extract_spectrum(const ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_mass_eigenstates&);
+   void write_to_file(const std::string&) const;
+
+private:
+   struct TParticle {
+      std::string name;
+      std::string latex_name;
+      std::valarray<double> masses;
+      TParticle(const std::string& name_, const std::string& latex_name_,
+                const std::valarray<double>& masses_)
+         : name(name_)
+         , latex_name(latex_name_)
+         , masses(masses_)
+         {}
+   };
+   using TSpectrum = std::vector<TParticle>;
+   TSpectrum spectrum{};
+   double scale{0.};
+   int width{16};
+
+   void write_spectrum(const TSpectrum&, std::ofstream&) const;
+};
+
+namespace ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_database {
+
+/// append parameter point to database
+void to_database(
+   const std::string&,
+   const ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_mass_eigenstates&,
+   const softsusy::QedQcd* qedqcd = nullptr,
+   const Physical_input* physical_input = nullptr,
+   const ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_observables* observables = nullptr);
+
+/// fill model from an entry of the database
+ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_mass_eigenstates from_database(
+   const std::string&,
+   long long,
+   softsusy::QedQcd* qedqcd = nullptr,
+   Physical_input* physical_input = nullptr,
+   ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_observables* observables = nullptr);
+
+} // namespace ScalarSingletZ2DMEWSBoutputlamHEFTHiggs_database
+
+} // namespace flexiblesusy
+
+#endif
