5fc9cadc29c4536d295da0eb8bbf7a18558d6360
Yang Zhang <yang.zhang@monash.edu>
2021-05-14 23:55:47 +0800

    Fix names of setting of pole_mass_loop_order in ScalarSingletZ2DMMhInputMsInput_withSingletVEVinPT

--- example/ScalarSingletZ2DMMhInputMsInput/ScalarSingletZ2DMMhInputMsInput_withSingletVEVinPT.hpp
+++ example/ScalarSingletZ2DMMhInputMsInput/ScalarSingletZ2DMMhInputMsInput_withSingletVEVinPT.hpp
@@ -177,8 +177,8 @@ void ScalarSingletZ2DMMhInputMsInput_withSingletVEVinPT::set_input(std::vector<d
   settings.set(Settings::precision, 1.e-8);
   settings.set(Settings::calculate_sm_masses, 1);
   
-  settings.set(Settings::poleMassLoopOrder, 1);
-  settings.set(Settings::ewsbLoopOrder, 1);
+  settings.set(Settings::pole_mass_loop_order, 1);
+  settings.set(Settings::ewsb_loop_order, 1);
   if (debug) {
     settings.set(Settings::threshold_corrections_loop_order,0);
     settings.set(Settings::beta_loop_order, 0);
