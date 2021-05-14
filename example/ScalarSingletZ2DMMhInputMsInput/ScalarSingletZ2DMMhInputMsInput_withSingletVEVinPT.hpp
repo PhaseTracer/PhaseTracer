8813440d92e05d07a25f618d6782559dd72bdf19
Yang Zhang <yang.zhang@monash.edu>
2021-05-14 09:41:59 +0800

    Fix loop order for ScalarSingletZ2DMMhInputMsInput

--- example/ScalarSingletZ2DMMhInputMsInput/ScalarSingletZ2DMMhInputMsInput_withSingletVEVinPT.hpp
+++ example/ScalarSingletZ2DMMhInputMsInput/ScalarSingletZ2DMMhInputMsInput_withSingletVEVinPT.hpp
@@ -176,6 +176,9 @@ void ScalarSingletZ2DMMhInputMsInput_withSingletVEVinPT::set_input(std::vector<d
   Settings settings;
   settings.set(Settings::precision, 1.e-8);
   settings.set(Settings::calculate_sm_masses, 1);
+  
+  settings.set(Settings::poleMassLoopOrder, 1);
+  settings.set(Settings::ewsbLoopOrder, 1);
   if (debug) {
     settings.set(Settings::threshold_corrections_loop_order,0);
     settings.set(Settings::beta_loop_order, 0);
