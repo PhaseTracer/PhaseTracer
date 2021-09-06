122be91a640ffef593c8b2a903fec4e7ea697486
Yang Zhang <yang.zhang@monash.edu>
2021-09-06 09:13:52 +0800

    Change inputs of run_xSM_HT.cp

--- example/xSM/run_xSM_HT.cpp
+++ example/xSM/run_xSM_HT.cpp
@@ -1,5 +1,8 @@
 /**
-  Example program for PhaseTracer.
+ Z2 real scalar singlet extension of
+ the Standard Model
+ 
+  High-temperature approximation
 
 */
 
@@ -14,121 +17,108 @@
 #include "logger.hpp"
 #include "phase_plotter.hpp"
 
-const std::string output_file_name = "xSM_HT_Results.txt";
+std::string toString(std::vector<double> in, std::vector<double> out) {
+  std::stringstream data_str;
+  for (auto i : in    ) data_str << i << "\t";
+  for (auto i : out   ) data_str << i << "\t";
+  return data_str.str();;
+}
 
 int main(int argc, char* argv[]) {
 
-  const bool debug_mode = argc == 1;
   std::ofstream output_file;
+  output_file.open("output.txt");
+
+  bool debug_mode = false;
+  double ms, lambda_s, lambda_hs;
   
-  double bins_lambda_hs;
-  double lambda_hs;
+  if ( argc == 1 ) {
+    debug_mode = true;
+    lambda_hs = 0.3;
+    ms = 65;
+    lambda_s =  0.1;
+  } else if ( argc >= 4 ) {
+    ms = atof(argv[1]);
+    lambda_s = atof(argv[2]);
+    lambda_hs = atof(argv[3]);
+  } else {
+    std::cout << "Use ./run_xSM_HT ms lambda_s lambda_hs" << std::endl;
+    return 0;
+  }
+  
   if (debug_mode){
     LOGGER(debug);
-    bins_lambda_hs = 1;
-    lambda_hs = 0.31;
+    std::cout << "ms = " << ms << std::endl
+              << "lambda_s = " << lambda_s << std::endl
+              << "lambda_hs = " << lambda_hs << std::endl;
   } else {
     LOGGER(fatal);
-    bins_lambda_hs = 100;
-    output_file.open(output_file_name);
   }
-
-  double lambda_s = 0.0;
-  double ms = SM::mh/2.;
-  
-  
-  lambda_hs = 0.4;
-  ms = 60;
-  lambda_s =  0.16;
-  
-  for (double ii = 0; ii < bins_lambda_hs; ii++) {
-    if (not debug_mode){
-      lambda_hs = 0.4 / bins_lambda_hs * ii+0.2;
-    }
     
-    // Construct our model
-    EffectivePotential::xSM_HT model(lambda_hs, lambda_s, ms);
-    if (debug_mode)
-      std::cout << model.check() << std::endl;
+  // Construct our model
+  EffectivePotential::xSM_HT model(lambda_hs, lambda_s, ms);
+  std::vector<double> in = {ms, lambda_s, lambda_hs};
+
+  
+  if (debug_mode){
+    std::cout << "Check = " << model.check() << std::endl;
+  }
+    
+  // Make PhaseFinder object and find the phases
+  PhaseTracer::PhaseFinder pf(model);
       
-    // Make PhaseFinder object and find the phases
-    PhaseTracer::PhaseFinder pf(model);
-      
-    pf.set_seed(0);
-    pf.set_check_hessian_singular(false);
+  pf.set_seed(0);
+  pf.set_check_hessian_singular(false);
+  
+  try {
     pf.find_phases();
-    if (debug_mode)
-      std::cout << pf;
-
-    // Make TransitionFinder object and find the transitions
-    PhaseTracer::TransitionFinder tf(pf);
-    tf.find_transitions();
-    if (debug_mode)
-      std::cout << tf;
-      
-    // Write results to data file
-    if (debug_mode) {
-      std::cout << lambda_hs << "\t"
-              << model.get_TC_from_expression() << "\t"
-              << model.get_vh_from_expression() << "\t"
-              << model.get_vs_from_expression() << "\t";
-    } else {
-//      output_file << lambda_hs << "\t"
-//              << model.get_TC_from_expression() << "\t"
-//              << model.get_vh_from_expression() << "\t"
-//              << model.get_vs_from_expression() << "\t";
-      
-      auto t = tf.get_transitions();
-      if (t.size()==0){
-        std::cout << "lambda_hs = " << lambda_hs << " found 0 transition!" << std::endl;
-        output_file << lambda_hs << "\t" << 0 << "\t";
-        output_file << -1 << "\t" << 0 << "\t"
-                    << 0 << "\t" << 0 << "\t"
-                    << 0 << "\t";
-        output_file << lambda_s << "\t" << ms;
-        output_file << std::endl;
-        continue;
-      }
-      int jj=0;
-      double gamme_max=0;
-      for (int i=0; i<t.size(); i++) {
-        double gamma = t[i].gamma;
-        if (gamme_max < gamma){
-          jj = i;
-          gamme_max = gamma;
-        }
-      }
-      output_file << lambda_hs << "\t" << t.size() << "\t";
-      output_file << t[jj].TC << "\t" << t[jj].true_vacuum[0] << "\t"
-                  << t[jj].true_vacuum[1] << "\t" << t[jj].false_vacuum[0] << "\t"
-                  << t[jj].false_vacuum[1] << "\t";
-      output_file << lambda_s << "\t" << ms;
-      output_file << std::endl;
-      
-    }
-      
-    auto trans = tf.get_transitions();
-    if (debug_mode) std::cout << trans.size() << "\t";
-    for (auto &t : tf.get_transitions()) {
-      if (debug_mode) {
-        std::cout << t.TC << "\t" << t.true_vacuum[0] << "\t"
-                  << t.true_vacuum[1] << "\t" << t.false_vacuum[0] << "\t"
-                  << t.false_vacuum[1] << "\t";
-//      }else{
-//        output_file << t.TC << "\t" << t.true_vacuum[0] << "\t"
-//                  << t.true_vacuum[1] << "\t" << t.false_vacuum[0] << "\t"
-//                  << t.false_vacuum[1] << "\t";
-      }
-    }
-    if (debug_mode) {
-      std::cout << std::endl;
-      // Print the data in a particular format for plotting
-      PhaseTracer::phase_plotter(tf, "xSM_HT");
-    } else {
-      output_file << std::endl;
-    }
-    
+  } catch (...) {
+    std::cout << "ms = " << ms << ",\t"
+              << "lambda_s = " << lambda_s << ",\t"
+              << "lambda_hs = " << lambda_hs << "\t"
+              << "encounters bug!" << std::endl;
+    std::vector<double> out = {-1, 0, 0, 0, 0, 0};
+    output_file << toString(in, out) << std::endl;
+    return 0;
   }
-  if (not debug_mode) output_file.close();
+  if (debug_mode) std::cout << pf;
+
+  // Make TransitionFinder object and find the transitions
+  PhaseTracer::TransitionFinder tf(pf);
+  tf.find_transitions();
+  if (debug_mode) std::cout << tf;
+      
+  auto t = tf.get_transitions();
+  if (t.size()==0){
+    std::cout << "ms = " << ms << ",\t"
+              << "lambda_s = " << lambda_s << ",\t"
+              << "lambda_hs = " << lambda_hs << "\t"
+              << "found 0 transition!" << std::endl;
+    std::vector<double> out = {-2, 0, 0, 0, 0, 0};
+    output_file << toString(in, out) << std::endl;
+    return 0;
+  }
+  
+  // Find the transition with largest gamma from (0,vs) -> (vh,0)
+  int jj = -1;
+  double gamme_max = 0.;
+  for (int i=0; i<t.size(); i++) {
+    double gamma = t[i].gamma;
+    if (gamme_max < gamma and abs(t[i].false_vacuum[0])<1. and abs(t[i].true_vacuum[1])<1.){
+      jj = i;
+      gamme_max = gamma;
+    }
+  }
+  
+  if (jj<0) {
+    std::vector<double> out = {-3, 0, 0, 0, 0, 0};
+    output_file << toString(in, out) << std::endl;
+    return 0;
+  }
+  
+  std::vector<double> out = {(float)t.size(), t[jj].TC, t[jj].true_vacuum[0], t[jj].true_vacuum[1], t[jj].false_vacuum[0], t[jj].false_vacuum[1]};
+  
+  output_file << toString(in, out) << std::endl;
+  output_file.close();
   return 0;
 }
