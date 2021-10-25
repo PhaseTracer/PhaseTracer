834306cac78c325b143a9cf16a0b85700205b9b1
Yang Zhang <yang.zhang@monash.edu>
2021-10-25 10:19:31 +0800

    Edit plot script

--- example/xSM/plot_scripts/1d_plot_line.py
+++ example/xSM/plot_scripts/1d_plot_line.py
@@ -7,8 +7,9 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 sys.path.append( '../' )
 from plot_fun import fun_gamma, fun_diff, loaddata
 
-plot_methods = True
-plot_xi = False
+plot_methods = False
+plot_Goldstone = False
+plot_xi = True
 
 cmap = cm.get_cmap('rainbow')
 
@@ -26,33 +27,60 @@ def plot_for_1d(data, x_num, label, column):
   ax = axs[1,column]
   ax.plot(x[sel], gamma[sel], label=label, alpha=1)
 
-if plot_methods:
-  
-#  fig, axs = plt.subplots(2, 3, figsize=(10, 6))
-#  names = [ ["default", "default"],
-#            ["2mt", r"$Q=2m_t$"],
-#            ["OSlike", r"OS-like"],
-#            ["xi3", r"$\xi=3$"],
-#            ["HT", r"HT"],
-#            ["PRM","PRM"],
-#            ["covariant_gauge", r"covariant, $\xi=3$"]]
+if plot_methods or plot_Goldstone:
+  if plot_methods:
+    
+    names = [ ["default", "default"],
+              ["2mt", r"$Q=2m_t$"],
+              ["OSlike", r"OS-like"],
+              ["xi3", r"$\xi=3$"],
+              ["HT", r"HT"],
+              ["PRM","PRM"],
+              ["covariant_gauge", r"covariant, $\xi=3$"]]
+    ncolumn = 3
+  if plot_Goldstone:
+    fig, axs = plt.subplots(2, 4, figsize=(13, 6))
+    names = [ ["default", "Goldstone resummation"],
+              ["1L_EWSB", "One-loop EWSB"] ]
+    ncolumn = 4
 
-  fig, axs = plt.subplots(2, 4, figsize=(13, 6))
-  names = [ ["default", "Goldstone resummation"],
-            ["1L_EWSB", "One-loop EWSB"] ]
-
+  fig, axs = plt.subplots(2, ncolumn, figsize=(10, 6))
 
   for name in names:
     plot_for_1d(np.loadtxt("../1d_bks/lambda_hs_"+name[0]+".txt"), 2, name[1], 0)
     plot_for_1d(np.loadtxt("../1d_bks/lambda_s_"+name[0]+".txt"), 1, name[1], 1)
     plot_for_1d(np.loadtxt("../1d_bks/m_s_"+name[0]+".txt"), 0, name[1], 2)
-  plot_for_1d(np.loadtxt("../1d_bks/Rxi_MSbar_resummation.txt"), 10, "Goldstone resummation", 3)
-  plot_for_1d(np.loadtxt("../1d_bks/Rxi_MSbar_1L_EWSB.txt"), 10, "One-loop EWSB", 3)
-  plot_for_1d(np.loadtxt("../1d_bks/Rxi_MSbar_no.txt"), 10, "Nothing", 3)
-    
+  
+  if plot_Goldstone:
+    plot_for_1d(np.loadtxt("../1d_bks/Rxi_MSbar_resummation.txt"), 10, "Goldstone resummation", 3)
+    plot_for_1d(np.loadtxt("../1d_bks/Rxi_MSbar_1L_EWSB.txt"), 10, "One-loop EWSB", 3)
+    plot_for_1d(np.loadtxt("../1d_bks/Rxi_MSbar_no.txt"), 10, "Nothing", 3)
 
-  for ii in range(2):
-    for jj in range(4):
+if plot_xi:
+  fig, axs = plt.subplots(2, 2, figsize=(10, 6))
+
+  names = [ ["MSbar", "MS"],
+            ["PRM", "PRM"],
+            ["PRM_0L", r"PRM(0-L)"]]
+  ncolumn = 2
+    
+  for name in names:
+    plot_for_1d(np.loadtxt("../1d_bks/Rxi_"+name[0]+".txt"), 10, name[1], 0)
+    plot_for_1d(np.loadtxt("../1d_bks/covariant_"+name[0]+".txt"), 10, name[1], 1)
+
+  data = np.loadtxt("../1d_bks/Rxi_HT.txt")
+  TC = data[:,4]
+  gamma = fun_gamma(data)
+  axs[0,0].plot([0,10], [TC[0],TC[-1]], label="HT", alpha=1)
+  axs[1,0].plot([0,10], [gamma[0],gamma[-1]], label="HT", alpha=1)
+  axs[0,1].plot([0,10], [TC[0],TC[-1]], label="HT", alpha=1)
+  axs[1,1].plot([0,10], [gamma[0],gamma[-1]], label="HT", alpha=1)
+
+  axs[0,0].set_title(r"R-$\xi$")
+  axs[0,1].set_title(r"Covariant")
+  
+for ii in range(2):
+    for jj in range(ncolumn):
       axs[ii,jj].grid(axis='x', alpha=0.75)
       axs[ii,jj].grid(axis='y', alpha=0.75)
       
@@ -63,37 +91,32 @@ if plot_methods:
         else:
           axs[ii,jj].legend(loc=4)
       else:
-        axs[ii,jj].set_ylim(0,10)
+#        axs[ii,jj].set_ylim(0,10)
         axs[ii,jj].set_ylabel(r"$\gamma_{\rm EW}$")
         if jj<1:
           axs[ii,jj].legend(loc=2)
         else:
           axs[ii,jj].legend(loc=1)
-          
-      if jj == 0:
-        axs[ii,jj].set_xlabel(r"$\lambda_{hs}$")
-      elif jj == 1:
-        axs[ii,jj].set_xlabel(r"$\lambda_{s}$")
-      elif jj == 2:
-        axs[ii,jj].set_xlabel(r"$m_{s}$ (GeV)")
+      
+      if plot_methods or plot_Goldstone:
+        if jj == 0:
+          axs[ii,jj].set_xlabel(r"$\lambda_{hs}$")
+        elif jj == 1:
+          axs[ii,jj].set_xlabel(r"$\lambda_{s}$")
+        elif jj == 2:
+          axs[ii,jj].set_xlabel(r"$m_{s}$ (GeV)")
+        else:
+          axs[ii,jj].set_xlabel(r"$\xi$")
       else:
         axs[ii,jj].set_xlabel(r"$\xi$")
 
-  fig.tight_layout()
-  plt.savefig('1d_bks.png')
+fig.tight_layout()
 
-if plot_xi:
-  fig, axs = plt.subplots(2, 2, figsize=(10, 6))
-
-  names = [ ["MSbar", "{MS}"],
-            ["PRM", "PRM"],
-            ["PRM_0L", r"PRM(0-L)"]]
-
-  for name in names:
-    plot_for_1d(np.loadtxt("../1d_bks/Rxi_"+name[0]+".txt"), 10, name[1], 0)
-    plot_for_1d(np.loadtxt("../1d_bks/covariant_"+name[0]+".txt"), 10, name[1], 1)
-
-
-
+if plot_methods:
+  plt.savefig('1d_methods.png')
+elif plot_Goldstone:
+  plt.savefig('1d_Goldstone.png')
+elif plot_xi:
+  plt.savefig('1d_xi.png')
 
 plt.show()
