04664af6eed9d20f4e21ff986815412e013e4236
Andrew Fowlie <andrew.j.fowlie@qq.com>
2021-11-08 14:29:07 +0800

    add settings to cov gauge and add tests

--- unit_tests/test_tadpoles.cpp
+++ unit_tests/test_tadpoles.cpp
@@ -3,7 +3,10 @@
 #include "models/SM_parameters.hpp"
 #include "logger.hpp"
 
-std::vector<double> tadpole_result(bool use_tree_level_tadpole, bool use_covariant_gauge) {
+std::vector<double> tadpole_result(bool use_covariant_gauge,
+                                   bool use_1L_EWSB_in_0L_mass,
+                                   bool use_Goldstone_resum,
+                                   bool use_tree_level_tadpole) {
   // Construct our model
   const double lambda_hs = 0.3;
   const double Q = SM::mtop;
@@ -17,7 +20,7 @@ std::vector<double> tadpole_result(bool use_tree_level_tadpole, bool use_covaria
   double lambda_s = lambda_s_min + 0.1;
 
   auto model = EffectivePotential::xSM_MSbar::from_tadpoles(lambda_hs, lambda_s, ms, Q, xi,
-              use_covariant_gauge, false, false, use_tree_level_tadpole);
+                use_covariant_gauge, use_1L_EWSB_in_0L_mass, use_Goldstone_resum, use_tree_level_tadpole);
 
   // Find singlet masses
   const auto mass_sq = use_tree_level_tadpole ? model.get_tree_scalar_masses_sq(physical) : model.get_1l_scalar_masses_sq(physical, 0.);
@@ -32,17 +35,31 @@ TEST_CASE("Z2 scalar singlet massses from tadpoles", "[TadpoleSolver]") {
 
   SECTION("covariant gauge") {
     for (bool use_tree_level_tadpole : {false, true}) {
-      const auto cov = tadpole_result(use_tree_level_tadpole, true);
-      CHECK(cov[0] == Approx(SM::mh));
-      CHECK(cov[1] == Approx(0.5 * SM::mh));
+      for (bool use_Goldstone_resum : {false, true}) {
+        for (bool use_1L_EWSB_in_0L_mass : {false, true}) {
+          INFO("1L in 0L mass = " + std::to_string(use_1L_EWSB_in_0L_mass)
+                + ". goldstone = " + std::to_string(use_Goldstone_resum)
+                + ". tree tadpole = " + std::to_string(use_tree_level_tadpole));
+          const auto rxi = tadpole_result(true, use_1L_EWSB_in_0L_mass, use_Goldstone_resum, use_tree_level_tadpole);
+          CHECK(rxi[0] == Approx(SM::mh));
+          CHECK(rxi[1] == Approx(0.5 * SM::mh));
+        }
+      }
     }
   }
 
   SECTION("R_xi gauge") {
     for (bool use_tree_level_tadpole : {false, true}) {
-      const auto rxi = tadpole_result(use_tree_level_tadpole, false);
-      CHECK(rxi[0] == Approx(SM::mh));
-      CHECK(rxi[1] == Approx(0.5 * SM::mh));
+      for (bool use_Goldstone_resum : {false, true}) {
+        for (bool use_1L_EWSB_in_0L_mass : {false, true}) {
+          INFO("1L in 0L mass = " + std::to_string(use_1L_EWSB_in_0L_mass)
+                + ". goldstone = " + std::to_string(use_Goldstone_resum)
+                + ". tree tadpole = " + std::to_string(use_tree_level_tadpole));
+          const auto rxi = tadpole_result(false, use_1L_EWSB_in_0L_mass, use_Goldstone_resum, use_tree_level_tadpole);
+          CHECK(rxi[0] == Approx(SM::mh));
+          CHECK(rxi[1] == Approx(0.5 * SM::mh));
+        }
+      }
     }
   }
 }
