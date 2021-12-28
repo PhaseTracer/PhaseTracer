d0b1ac4e64515321859b804e817e44ef8c6d1b94
Yang Zhang <yang.phy@foxmail.com>
2021-12-28 16:29:10 +0800

    Edit plot script and data

--- example/xSM/plot_scripts/1d_plot_band.py
+++ example/xSM/plot_scripts/1d_plot_band.py
@@ -12,50 +12,69 @@ cmap = cm.get_cmap('rainbow')
 plot_scale = True
 plot_xi = False
 
-def line_for_1d(data, x_num, color, column, label = ""):
-  sel = data[:,3]>0
-
+def selection(for_TC, x_num, data):
+  if for_TC:
+    sel = (data[:,3]>0) & (data[:,4]<200.)
+    y = data[:,4][sel]
+  else:
+    sel = (data[:,3]>0) & (fun_gamma(data)>0)
+    y = fun_gamma(data)[sel]
   x = data[:,x_num][sel]
-  TC = data[:,4][sel]
-  gamma = fun_gamma(data)[sel]
+  return x,y
 
+def line_for_1d(data, x_num, color, column, label = "", linestyle="-", alpha=1):
+  x_TC, TC = selection(True, x_num, data)
+  x_gamma, gamma = selection(False, x_num, data)
+  
   ax = axs[0,column]
   if label == "":
-    ax.plot(x, TC, color=color, alpha=1)
+    ax.plot(x_TC, TC, color=color, linestyle=linestyle, alpha=alpha)
   else:
-    ax.plot(x, TC, color=color, alpha=1, label=label)
+    ax.plot(x_TC, TC, color=color, linestyle=linestyle, alpha=alpha, label=label)
     
   ax = axs[1,column]
   if label == "":
-    ax.plot(x, gamma, color=color, alpha=1)
+    ax.plot(x_gamma, gamma, color=color, linestyle=linestyle, alpha=alpha)
   else:
-    ax.plot(x, gamma, color=color, alpha=1, label=label)
+    ax.plot(x_gamma, gamma, color=color, linestyle=linestyle, alpha=alpha, label=label)
     
-    
+def interpolate(x1, y1, x2, y2):
+  if min(x1) < min(x2):
+    x2 = np.append(x1[0],x2)
+    y2 = np.append(y1[0],y2)
+  elif min(x1) > min(x2):
+    x1 = np.append(x2[0],x1)
+    y1 = np.append(y2[0],y1)
+  if max(x1) > max(x2):
+    x2 = np.append(x1[-1],x2)
+    y2 = np.append(y1[-1],y2)
+  elif max(x1) < max(x2):
+    x1 = np.append(x2[-1],x1)
+    y1 = np.append(y2[-1],y1)
+
+  f1 = interp1d(x1, y1, kind='linear')
+  f2 = interp1d(x2, y2, kind='linear')
+  return f1, f2  
+  
 def range_for_1d(data1, data2, x_num, label, column, color):
 
-  sel1 = data1[:,3]>0
-  x1 = data1[:,x_num][sel1]
-  TC1 = data1[:,4][sel1]
-  gamma1 = fun_gamma(data1)[sel1]
+  x_TC1, TC1 = selection(True, x_num, data1)
+  x_TC2, TC2 = selection(True, x_num, data2)
 
-  sel2 = data2[:,3]>0
-  x2 = data2[:,x_num][sel2]
-  TC2 = data2[:,4][sel2]
-  gamma2 = fun_gamma(data2)[sel2]
+  x_gamma1, gamma1 = selection(False, x_num, data1)
+  x_gamma2, gamma2 = selection(False, x_num, data2)
 
-  fTC1 = interp1d(x1, TC1, kind='linear')
-  fTC2 = interp1d(x2, TC2, kind='linear')
-  fgamma1 = interp1d(x1, gamma1, kind='linear')
-  fgamma2 = interp1d(x2, gamma2, kind='linear')
+  fTC1, fTC2 = interpolate(x_TC1, TC1, x_TC2, TC2)
+  fgamma1, fgamma2 = interpolate(x_gamma1, gamma1, x_gamma2, gamma2)
   
-  x = np.linspace(max(min(x1),min(x2)), min(max(x1),max(x2)), num=50, endpoint=True)
+  x_TC = np.linspace(min(min(x_TC1),min(x_TC2)), max(max(x_TC1),max(x_TC2)), num=50, endpoint=True)
+  x_gamma = np.linspace(min(min(x_gamma1),min(x_gamma2)), max(max(x_gamma1),max(x_gamma2)), num=50, endpoint=True)
 
   ax = axs[0,column]
-  ax.fill_between(x,fTC1(x),fTC2(x), color=color, alpha=0.3, linewidth=0, label=label)
+  ax.fill_between(x_TC,fTC1(x_TC),fTC2(x_TC), color=color, alpha=0.3, linewidth=0, label=label)
   
   ax = axs[1,column]
-  ax.fill_between(x,fgamma1(x),fgamma2(x), color=color, alpha=0.3, linewidth=0, label=label)
+  ax.fill_between(x_gamma,fgamma1(x_gamma),fgamma2(x_gamma), color=color, alpha=0.3, linewidth=0, label=label)
 
 fig, axs = plt.subplots(2, 3, figsize=(12, 6))
 
@@ -74,34 +93,37 @@ for name in [["m_s",0,2], ["lambda_s",1,1], ["lambda_hs",2,0],]:
 #                 np.loadtxt("../1d_bks/"+name[0]+"_noRGE_2mt.txt"),
 #                 name[1], r"MS(no RGE), $Q\in[m_t/2,2m_t]$", name[2], 'purple')
 
-    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_mt.txt"), name[1], "k", name[2])
-    range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_05mt.txt"),
-                 np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_2mt.txt"),
-                 name[1], r"PRM(No RGE), $Q\in[m_t/2,2m_t]$", name[2], 'k')
+
+
+    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_mt.txt"), name[1], "g", name[2])
+    range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_05mt.txt"),
+                 np.loadtxt("../1d_bks/"+name[0]+"_2mt.txt"),
+                 name[1], r"$\overline{\rm MS}$", name[2], 'g')
                  
-    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_noRGE_mt.txt"), name[1], "yellow", name[2])
-    range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_noRGE_05mt.txt"),
-                 np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_noRGE_2mt.txt"),
-                 name[1], r"PRM(No RGE, FS), $Q\in[m_t/2,2m_t]$", name[2], 'yellow')
+                
+    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_noD_mt.txt"), name[1], "b", name[2])
+    range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_noD_05mt.txt"),
+                 np.loadtxt("../1d_bks/"+name[0]+"_noD_2mt.txt"),
+                 name[1], r"$\overline{\rm MS}$(no daisy)", name[2], 'b')
+                 
 
     line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_mt.txt"), name[1], "r", name[2])
     range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_05mt.txt"),
                  np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_2mt.txt"),
-                 name[1], r"PRM, $Q\in[m_t/2,2m_t]$", name[2], 'r')
+                 name[1], r"PRM", name[2], 'r')
+                                  
+    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_noRGE_mt.txt"), name[1], "purple", name[2])
+    range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_noRGE_05mt.txt"),
+                 np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_noRGE_2mt.txt"),
+                 name[1], r"PRM(no RGE)", name[2], 'purple')
+    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_noRGE_2mt.txt"), name[1], "purple", name[2], linestyle= "--", alpha=0.4)
                  
-    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_mt.txt"), name[1], "g", name[2])
-    range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_05mt.txt"),
-                 np.loadtxt("../1d_bks/"+name[0]+"_2mt.txt"),
-                 name[1], r"MS, $Q\in[m_t/2,2m_t]$", name[2], 'g')
-                 
-                 
-                 
-    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_noD_mt.txt"), name[1], "b", name[2])
-    range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_noD_05mt.txt"),
-                 np.loadtxt("../1d_bks/"+name[0]+"_noD_2mt.txt"),
-                 name[1], r"MS(no daisy), $Q\in[m_t/2,2m_t]$", name[2], 'b')
+#    line_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_mt.txt"), name[1], "k", name[2])
+#    range_for_1d(np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_05mt.txt"),
+#                 np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_2mt.txt"),
+#                 name[1], r"PRM(No RGE woFS), $Q\in[m_t/2,2m_t]$", name[2], 'k')
 
-
+                 
 
 
                  
@@ -130,25 +152,29 @@ for ii in range(2):
     axs[ii,jj].grid(axis='y', alpha=0.75)
     
     if ii == 0:
+      axs[ii,jj].set_ylim(40,170)
       axs[ii,jj].set_ylabel(r"$T_C$ (GeV)")
 #      if jj<1:
 #        axs[ii,jj].legend(loc=3)
 #      else:
 #        axs[ii,jj].legend(loc=4)
     else:
-      axs[ii,jj].set_ylim(0,10)
+      axs[ii,jj].set_ylim(0,6)
       axs[ii,jj].set_ylabel(r"$\gamma_{\rm EW}$")
       if jj<1:
         axs[ii,jj].legend(loc=2)
       else:
         axs[ii,jj].legend(loc=1)
-        
+    
     if jj == 0:
       axs[ii,jj].set_xlabel(r"$\lambda_{hs}$")
+      axs[ii,jj].set_xlim(0.1,0.4)
     elif jj == 1:
       axs[ii,jj].set_xlabel(r"$\lambda_{s}$")
+      axs[ii,jj].set_xlim(0.05,0.2)
     else:
       axs[ii,jj].set_xlabel(r"$m_{s}$ (GeV)")
+      axs[ii,jj].set_xlim(40,100)
 
 fig.tight_layout()
 
