f8d3365b4317ffcedf6e580e0707f4c8dae4e374
phyzhangyang <yang.phy@foxmail.com>
2022-04-28 15:57:10 +0800

    Edit plot script

--- example/xSM/plot_scripts/3d_mu.py
+++ example/xSM/plot_scripts/3d_mu.py
@@ -20,8 +20,8 @@ def make_plot(plot_type):
 
   style()
     
-  fig, axs = plt.subplots(1, 3, figsize=(15, 5))
   if plot_type == "scatter":
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
     diff = fun_diff(data_mu_2, data_mu_05, data_mu_1, show_gamma=False, norm =False, sort=True, use_abs = False)
     vmax = 20
     size = 20
@@ -45,52 +45,68 @@ def make_plot(plot_type):
         fig = plt.gcf()
         fig.subplots_adjust(right=0.9, wspace=0.3, bottom=0.125)
             
-        cbar_ax = fig.add_axes([0.915, 0.15, 0.02, 0.7])
+        cbar_ax = fig.add_axes([0.915, 0.50, 0.02, 0.4])
         fig.colorbar(map, cax=cbar_ax)
-              
-    make_plot(axs[0], 1, 2)
-    make_plot(axs[1], 0, 1)
-    make_plot(axs[2], 0, 2, cbar=True)
+
+    def no_color(ax, nn):
+      ax.grid(axis='x', alpha=0.75)
+      ax.grid(axis='y', alpha=0.75)
+      if nn == 0 :
+        diff = fun_diff(data_mu_2, data_mu_05, data_mu_1, show_gamma=False, norm =False, use_abs = False)
+        ax.scatter(diff[:,5], diff[:,4], c='seagreen', alpha=1, edgecolor='none', rasterized=True)
+        ax.set_xlabel(r'$T_C(Q=m_t)$')
+        ax.set_ylabel(r'$T_C(Q=\frac{1}{2}m_t)-T_C(Q=2m_t)$')
+      elif nn == 1 :
+        diff = fun_diff(data_mu_2, data_mu_05, data_mu_1, show_gamma=False, norm =True, use_abs = False)
+        ax.scatter(diff[:,5], diff[:,4], c='seagreen', alpha=1, edgecolor='none', rasterized=True)
+        ax.set_xlabel(r'$T_C(Q=m_t)$')
+        ax.set_ylabel(r'$[T_C(Q=\frac{1}{2}m_t)-T_C(Q=2m_t)]/T_C(Q=m_t)$')
+      else:
+        diff = fun_diff(data_mu_2, data_mu_05, data_mu_1, show_gamma=True, norm =True, use_abs = False)
+        ax.scatter(diff[:,5], diff[:,4], c='seagreen', alpha=1, edgecolor='none', rasterized=True)
+        ax.set_xlabel(r'$\gamma(Q=m_t)$')
+        ax.set_ylabel(r'$[\gamma(Q=\frac{1}{2}m_t)-\gamma(Q=2m_t)]/\gamma(Q=m_t)$')
+
+            
+    make_plot(axs[0,0], 1, 2)
+    make_plot(axs[0,1], 0, 1)
+    make_plot(axs[0,2], 0, 2, cbar=True)
+    no_color(axs[1,0], 0)
+    no_color(axs[1,1], 1)
+    no_color(axs[1,2], 2)
     plt.savefig('3d_mu_scatter.pdf')
   
   else:
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    
     show_gamma = plot_type == "gamma"
-
-  
-    ax = axs[0]
-    diff = fun_diff(data_mu_2, data_mu_05, data_mu_1, show_gamma=show_gamma, norm =False, use_abs = False)
-    ax.grid(axis='y', alpha=0.75)
-    ax.hist(x=diff[:,4], bins=30, color='seagreen', log=True, alpha=1, rwidth=0.85)
-    if show_gamma:
-      ax.set_xlabel(r'$\gamma(Q=\frac{1}{2}m_t)-\gamma(Q=2m_t)$')
-    else:
-      ax.set_xlabel(r'$T_C(Q=\frac{1}{2}m_t)-T_C(Q=2m_t)$')
+    def hist_plot(nn):
+      show_gamma = (nn==1)
+      ax = axs[nn,0]
+      diff = fun_diff(data_mu_2, data_mu_05, data_mu_1, show_gamma=show_gamma, norm =False, use_abs = False)
+      ax.grid(axis='y', alpha=0.75)
+      ax.hist(x=diff[:,4], bins=30, color='seagreen', log=True, alpha=1, rwidth=0.85)
+      if show_gamma:
+        ax.set_xlabel(r'$\gamma(Q=\frac{1}{2}m_t)-\gamma(Q=2m_t)$')
+      else:
+        ax.set_xlabel(r'$T_C(Q=\frac{1}{2}m_t)-T_C(Q=2m_t)$')
       
-    ax.set_ylabel("Number of samples")
+      ax.set_ylabel("Number of samples")
     
-    ax = axs[1]
-    diff = fun_diff(data_mu_2, data_mu_05, data_mu_1, show_gamma=show_gamma, norm =True, use_abs = False)
-    ax.grid(axis='y', alpha=0.75)
-    ax.hist(x=diff[:,4], bins=30, color='seagreen', log=True, alpha=1, rwidth=0.85)
-    if show_gamma:
-      ax.set_xlabel(r'$[\gamma(Q=\frac{1}{2}m_t)-\gamma(Q=2m_t)]/\gamma(Q=m_t)$')
-    else:
-      ax.set_xlabel(r'$[T_C(Q=\frac{1}{2}m_t)-T_C(Q=2m_t)]/T_C(Q=m_t)$')
-    ax.set_ylabel("Number of samples")
+      ax = axs[nn,1]
+      diff = fun_diff(data_mu_2, data_mu_05, data_mu_1, show_gamma=show_gamma, norm =True, use_abs = False)
+      ax.grid(axis='y', alpha=0.75)
+      ax.hist(x=diff[:,4], bins=30, color='seagreen', log=True, alpha=1, rwidth=0.85)
+      if show_gamma:
+        ax.set_xlabel(r'$[\gamma(Q=\frac{1}{2}m_t)-\gamma(Q=2m_t)]/\gamma(Q=m_t)$')
+      else:
+        ax.set_xlabel(r'$[T_C(Q=\frac{1}{2}m_t)-T_C(Q=2m_t)]/T_C(Q=m_t)$')
+      ax.set_ylabel("Number of samples")
     
-    ax = axs[2]
-    ax.grid(axis='x', alpha=0.75)
-    ax.grid(axis='y', alpha=0.75)
-    ax.scatter(diff[:,5], diff[:,4], c='seagreen', alpha=1, edgecolor='none', rasterized=True)
-    if show_gamma:
-      ax.set_xlabel(r'$\gamma(Q=m_t)$')
-      ax.set_ylabel(r'$[\gamma(Q=\frac{1}{2}m_t)-\gamma(Q=2m_t)]/\gamma(Q=m_t)$')
-    else:
-      ax.set_xlabel(r'$T_C(Q=m_t)$')
-      ax.set_ylabel(r'$[T_C(Q=\frac{1}{2}m_t)-T_C(Q=2m_t)]/T_C(Q=m_t)$')
-    
+    hist_plot(0)
+    hist_plot(1)
     fig.tight_layout()
-    plt.savefig('3d_mu_hist'+ ('_gamma' if show_gamma else '_TC') +'.pdf')
+    plt.savefig('3d_mu_hist.pdf')
   
   plt.show()
  
