ca579a84fdf39e7f3a24b8b568650cbe8e38dda6
Yang Zhang <yang.phy@foxmail.com>
2022-06-21 16:04:22 +0800

    Add scripts and result for Q~(0.5T,2piT)

--- example/xSM/plot_scripts/1d_plot_band.py
+++ example/xSM/plot_scripts/1d_plot_band.py
@@ -14,11 +14,20 @@ from style import style
 
 style()
 
+
+plot_scale = False
+
 def selection(for_TC, x_num, data, PRM=False):
     x = data[:, x_num]
 
     if for_TC:
+      if plot_scale:
         sel = ( (data[:,3]>0) | (PRM&(data[:, 3]>=0)) ) & (data[:, 4] < 200.) 
+        y = data[:, 4]
+      else:
+        sel = ( (data[:,3]>0) | (PRM&(data[:, 3]>=0)) ) & (data[:, 4] < 200.) 
+        Q_sel = ( abs(data[:,9]/data[:,4] - 3.1415926*2) < 0.01 ) | ( abs(data[:,9]/data[:,4] - 0.5) < 0.01 ) | ( abs(data[:,9]/data[:,4] - 1) < 0.01 ) 
+        sel = sel & Q_sel
         y = data[:, 4]
     else:
         sel = ( (data[:,3]>0) | (PRM &(data[:, 3]>=0)) )
@@ -36,8 +45,9 @@ def line_for_1d(axs, data, x_num, color, column, label="_nolegend_", linestyle="
     ax = axs[0, column]
     ax.plot(x_TC, TC, color=color, linestyle=linestyle, alpha=alpha, label=label)
 
-    ax = axs[2, column]
-    ax.plot(x_gamma, gamma, color=color, linestyle=linestyle, alpha=alpha, label=label)
+    if plot_scale:
+      ax = axs[2, column]
+      ax.plot(x_gamma, gamma, color=color, linestyle=linestyle, alpha=alpha, label=label)
 
     fTC = interp1d(x_TC, TC, kind='linear', bounds_error=False, fill_value='extrapolate')
     fgamma = interp1d(x_gamma, gamma, kind='linear', bounds_error=False, fill_value='extrapolate')
@@ -97,19 +107,21 @@ def range_for_1d(axs, data1, data2, x_num, label, column, color, fTC, fgamma, PR
     ax = axs[0,column]
     ax.fill_between(x_TC, lTC, uTC, color=color, alpha=0.3, linewidth=0, label=label)
 
-    lgamma, ugamma, cgamma, dgamma = lucd(x_gamma, fgamma1, fgamma2, fgamma)
-    ax = axs[2,column]
-    ax.fill_between(x_gamma, lgamma, ugamma, color=color, alpha=0.3, linewidth=0, label=label)
+    if plot_scale:
+      lgamma, ugamma, cgamma, dgamma = lucd(x_gamma, fgamma1, fgamma2, fgamma)
+      ax = axs[2,column]
+      ax.fill_between(x_gamma, lgamma, ugamma, color=color, alpha=0.3, linewidth=0, label=label)
 
     x_TC = np.linspace(max(min(x_TC1), min(x_TC2)), min(max(x_TC1), max(x_TC2)), num=100, endpoint=True)
     lTC, uTC, cTC, dTC = lucd(x_TC, fTC1, fTC2, fTC)
     ax = axs[1,column]
     ax.plot(x_TC, dTC/cTC, color=color, label=label)
 
-    x_gamma = np.linspace(max(min(x_gamma1), min(x_gamma2)), min(max(x_gamma1), max(x_gamma2)), num=100, endpoint=True)
-    lgamma, ugamma, cgamma, dgamma = lucd(x_gamma, fgamma1, fgamma2, fgamma)
-    ax = axs[3,column]
-    ax.plot(x_gamma, dgamma / cgamma, color=color, label=label)
+    if plot_scale:
+      x_gamma = np.linspace(max(min(x_gamma1), min(x_gamma2)), min(max(x_gamma1), max(x_gamma2)), num=100, endpoint=True)
+      lgamma, ugamma, cgamma, dgamma = lucd(x_gamma, fgamma1, fgamma2, fgamma)
+      ax = axs[3,column]
+      ax.plot(x_gamma, dgamma / cgamma, color=color, label=label)
 
 #    ax = axs[0,column]
 #    ax.fill_between(x_TC,fTC1(x_TC),fTC2(x_TC), color=color, alpha=0.3, linewidth=0, label=label)
@@ -133,12 +145,15 @@ def make_plot(plot_type):
 
     if plot_type == "scale":
         plot_scale = True
-    elif plot_type == "xi":
+    elif plot_type == "scaleT":
         plot_scale = False
     else:
         raise RuntimeError("unknown plot type")
 
-    fig, axs = plt.subplots(4, 3, figsize=(11, 12), sharey="row", sharex="col")
+    if plot_scale:
+      fig, axs = plt.subplots(4, 3, figsize=(11, 12), sharey="row", sharex="col")
+    else:
+      fig, axs = plt.subplots(2, 3, figsize=(11, 6), sharey="row", sharex="col")
     plt.subplots_adjust(hspace=0.1, wspace=0.1)
 
     for name in [["m_s", 0, 2], ["lambda_s", 1, 1], ["lambda_hs", 2, 0]]:
@@ -177,25 +192,40 @@ def make_plot(plot_type):
         fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_2mt.txt"), name[1], "purple", name[2], linestyle="--", alpha=0.4, PRM=True)
 
       else:
-        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_xi1.txt"), name[1], "g", name[2])
-        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_xi0.txt"),
-                     np.loadtxt("../1d_bks/"+name[0]+"_xi3.txt"),
-                     name[1], r"MS, $xi\in[0,3]$", name[2], 'g', fTC, fgamma)
+      
+        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_T.txt"), name[1], "purple", name[2])
+        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_05T.txt"),
+                     np.loadtxt("../1d_bks/"+name[0]+"_2piT.txt"),
+                     name[1], r"$\overline{\rm MS}$ w AE", name[2], 'purple', fTC, fgamma)
+                     
+        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PW_T.txt"), name[1], "g", name[2])
+        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PW_05T.txt"),
+                     np.loadtxt("../1d_bks/"+name[0]+"_PW_2piT.txt"),
+                     name[1], r"$\overline{\rm MS}$ w PW", name[2], 'g', fTC, fgamma)
 
-        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_noD_xi1.txt"), name[1], "r", name[2])
-        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_noD_xi0.txt"),
-                     np.loadtxt("../1d_bks/"+name[0]+"_noD_xi3.txt"),
-                     name[1], r"MS(no daisy), $xi\in[0,3]$", name[2], 'r', fTC, fgamma)
+        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_noD_T.txt"), name[1], "b", name[2])
+        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_noD_05T.txt"),
+                     np.loadtxt("../1d_bks/"+name[0]+"_noD_2piT.txt"),
+                     name[1], r"$\overline{\rm MS}$ w/o daisy", name[2], 'b', fTC, fgamma)
 
-        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_xi1.txt"), name[1], "b", name[2])
-        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_xi0.txt"),
-                     np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_xi3.txt"),
-                     name[1], r"PRM(1L), $xi\in[0,3]$", name[2], 'b', fTC, fgamma)
+        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_noRGE_woFS_T.txt"), name[1], "gray", name[2])
+        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_noRGE_woFS_05T.txt"),
+                     np.loadtxt("../1d_bks/"+name[0]+"_noRGE_woFS_2piT.txt"),
+                     name[1], r"$\overline{\rm MS}$ w/o RGE", name[2], 'gray', fTC, fgamma)
 
-        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_0L.txt"), name[1], "k", name[2], r"PRM(0L)")
+        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_T.txt"), name[1], "r", name[2], PRM=True)
+        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_05T.txt"),
+                     np.loadtxt("../1d_bks/"+name[0]+"_PRM_0L_2piT.txt"),
+                     name[1], r"PRM", name[2], 'r', fTC, fgamma, PRM=True)
+
+        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_T.txt"), name[1], "orange", name[2], PRM=True)
+        range_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_05T.txt"),
+                     np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_2piT.txt"),
+                     name[1], r"PRM w/o RGE", name[2], 'orange', fTC, fgamma, PRM=True)
+        fTC, fgamma = line_for_1d(axs, np.loadtxt("../1d_bks/"+name[0]+"_PRM_woFS_noRGE_2piT.txt"), name[1], "purple", name[2], linestyle="--", alpha=0.4, PRM=True)
 
 
-    for ii in range(4):
+    for ii in range(4 if plot_scale else 2):
       for jj in range(3):
         axs[ii, jj].grid(axis='x', alpha=0.5)
         axs[ii, jj].grid(axis='y', alpha=0.5)
@@ -229,14 +259,14 @@ def make_plot(plot_type):
       plt.figlegend(handles, labels, ncol=3, loc="upper center", bbox_to_anchor=(0.115, 0.75, 0.795, 0.2))
       plt.subplots_adjust(top=0.87)
     else:
-      axs[3, 0].legend(loc=3, framealpha=0.7)
-      axs[2, 0].legend(loc=2, framealpha=0.7)
-      fig.tight_layout()
+      handles, labels = axs[1, 1].get_legend_handles_labels()
+      plt.figlegend(handles, labels, ncol=3, loc="upper center", bbox_to_anchor=(0.115, 0.80, 0.795, 0.2))
+      plt.subplots_adjust(top=0.85)
 
     if plot_scale:
       plt.savefig('1d_scale.pdf')
     else:
-      plt.savefig('1d_xi.png')
+      plt.savefig('1d_scale_T.pdf')
 
 if __name__ == "__main__":
     make_plot()
