a590ccf776e0565bb712edc0d810459d95fc19ca
phyzhangyang <yang.phy@foxmail.com>
2024-03-11 15:05:41 +0800

    Tidy the examples for shooting and pathdeformation

--- example/TestShooting/run_shooting_1d.cpp
+++ example/TestShooting/run_shooting_1d.cpp
@@ -1,6 +1,5 @@
 /**
- The scale example BubbleProfiler
- ./run_BP_scale 1. 0.6 200.
+ The 1d shooting example in CosmoTransitions
 */
 
 #include <fstream>
@@ -12,7 +11,6 @@
 #include "transition_finder.hpp"
 #include "logger.hpp"
 #include "shooting.hpp"
-#include "path_deformation.hpp"
 
 
 class Example: public PhaseTracer::PotentialForShooting {
@@ -22,19 +20,15 @@ public:
   
   double V(double phi) const override {
     return k1*pow(phi,4) - k2*pow(phi,3) + k3*pow(phi,2);
-//    return 0.25*pow(phi,4) - 0.4*pow(phi,3) + 0.1 * pow(phi,2);
   }
 
   
   double dV(double phi) const override {
     return 4.*k1*pow(phi,3) - 3.*k2*pow(phi,2) + 2.*k3*phi;
-//    return phi*(phi-.2)*(phi-1);
   }
   
   double d2V(double phi) const override {
     return 12.*k1*pow(phi,2) - 6.*k2*phi + 2.*k3;
-//    return (phi-.47)*(phi-1) + phi*(phi-1) + phis*(phi-.47);
-//    return (phi-.2)*(phi-1) + phi*(phi-1) + phi*(phi-.2);
   }
   
 private:
@@ -45,67 +39,31 @@ private:
 
 int main(int argc, char* argv[]) {
 
-  LOGGER(debug);
-  
-  Example p(0.25,0.49,0.235);
-//  Example p(0.25,0.4,0.1);
-//
-  PhaseTracer::Shooting s(p);
-//
-  auto profile = s.findProfile(0,1);
-  
-  std::ofstream file("test_data.txt");
-  for (int jj=0; jj< profile.R.size(); jj++){
-//    std::cout << "R, phi, dphi = " << profile.R[jj] << ", " << profile.Phi(jj) << ", "  << profile.dPhi(jj) << std::endl;
-    file << profile.R[jj] << ", " << profile.Phi(jj) << ", "  << profile.dPhi(jj) << std::endl;
-  }
-  file.close();
-
-  auto action = s.calAction(profile);
-  std::cout << "action = " << std::setprecision(8) << action << std::endl;
-//
-//  PhaseTracer::PathDeformation pd(0);
-//  auto a = pd.fullTunneling(0);
-//
-  
-  
-//  double E, alpha, scale;
-//
-//  if ( argc == 4 ) {
-//    E = atof(argv[1]);
-//    alpha = atof(argv[2]);
-//    scale = atof(argv[3]);
-//  } else {
-//    std::cout << "Use ./run_BP_scale E alpha scale" << std::endl;
-//    return 0;
-//  }
-//
+  LOGGER(fatal);
 //  LOGGER(debug);
-//
-//  // Construct our model
-//  EffectivePotential::BP_scale model(E, alpha, scale);
-//
-//  // Make PhaseFinder object and find the phases
-//  PhaseTracer::PhaseFinder pf(model);
-////  pf.set_seed(0);
-//  pf.set_check_hessian_singular(false);
-//  pf.set_check_vacuum_at_high(false);
-//  pf.set_guess_points({Eigen::VectorXd::Zero(1)});
-//
-//  pf.find_phases();
-//  std::cout << pf;
-//
-//  // Make TransitionFinder object and find the transitions
-//  PhaseTracer::TransitionFinder tf(pf);
-////  tf.find_transitions();
-////  std::cout << tf;
-//
-//  const auto phases = pf.get_phases();
-//  auto phase1 = phases[1];
-//  auto phase2 = phases[0];
-//  auto s = tf.get_action(phase1, phase2, 0);
-//  auto vacua = tf.get_vacua_at_T(phase1, phase2, 0);
-//  std::cout << vacua[0][0] << " " << vacua[1][0] << " " << s << std::endl;
-//
+  
+  double k2_min = 0.4;  // Thick-walled
+  double k3_min = 0.1;
+  double k2_max = 0.49; // Thin-walled
+  double k3_max = 0.235;
+  
+  int num_values = 10;
+  std::vector<double> k2_values;
+  std::vector<double> k3_values;
+  double k1 = 0.25;
+
+  for (int i = 0; i < num_values; ++i) {
+    double k2 = k2_min + i * (k2_max - k2_min) / (num_values - 1);
+    double k3 = k3_min + i * (k3_max - k3_min) / (num_values - 1);
+
+    Example p(k1,k2,k3);
+    PhaseTracer::Shooting s(p);
+    auto profile = s.findProfile(0,1);
+    auto action = s.calAction(profile);
+
+    std::cout << "k2= "<< k2 << ", action = " << std::setprecision(10) << action << std::endl;
+
+  }
+  
   return 0;
 }
