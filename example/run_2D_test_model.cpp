cbd99866d0ded893acc0b37c19404bf175efbe97
Yang Zhang <yang.phy@foxmail.com>
2024-07-03 18:02:48 +0800

    Add Tn calculation in the scan

--- example/run_2D_test_model.cpp
+++ example/run_2D_test_model.cpp
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]) {
   pf.find_phases();
   std::cout << pf;
   
-  // Make PhaseFinder object
+  // Make ActionCalculator object
   PhaseTracer::ActionCalculator ac(model);
   ac.set_use_BubbleProfiler(false);
   
