84a7d596d497dff0f3e98dee63259a12bf840c4a
phyzhangyang <yang.phy@foxmail.com>
2024-07-10 23:25:38 +0800

    Edit gravwave_calculator

--- include/gravwave_calculator.hpp
+++ include/gravwave_calculator.hpp
@@ -10,135 +10,137 @@
 
 namespace PhaseTracer {
 
-
-struct GravWavetrans_params {
+struct GravWaveSpectrum {
+  double Tref;
   double alpha;
-  double beta_H;
-  double T_ref;
+  double beta;
+  double peak_frequency;
+  double peak_amplitude;
+  std::vector<double> frequency;
+  std::vector<double> sound_wave;
+  std::vector<double> turbulence;
+  std::vector<double> bubble_collision;
+  std::vector<double> total_amplitude;
+  
+  /** Pretty-printer for GravWaveSpectrum */
+  friend std::ostream& operator << (std::ostream& o, const GravWaveSpectrum& a) {
+    o << "=== Gravitational wave generated at T = " << a.Tref << std::endl;
+    o << "    alpha = " << a.alpha << std::endl
+      << "    beta = " << a.beta << std::endl
+      << "    peak_frequency = " << a.peak_frequency << std::endl
+      << "    peak_amplitude = " << a.peak_amplitude << std::endl;
+    return o;
+  }
 };
 
+
 class GravWaveCalculator {
-private:
-    std::vector<double> frequency_list;
-    std::vector<GravWavetrans_params> transitions_params;
-    PROPERTY(int, num_random_iterations, 5)
-    PROPERTY(double, dof, 106.75)
-    PROPERTY(double, epsilon, 0.1)
-    /** The step-size in numerical derivatives */
-    PROPERTY(double, h, 1e-2)
-    /** The number of points used in numerical derivative of action */
-    PROPERTY(double, np_dSdT, 5)
-  
-    PROPERTY(double, vw, 0.3)
-    const double G = 6.7088e-39;
-    TransitionFinder tf;
-    
-    const double rho_R(double T){
-      return std::pow(T, 4) * M_PI * M_PI * dof / 30.;
-    }
-
-    const double H(double T){
-      return std::sqrt(8. * M_PI * G / 3. * rho_R(T));
-    }
-
-    /* Calcualte alpha with fixed phi */
-    const double V(const Eigen::VectorXd phi, const double T) {
-      return tf.pf.P.V(phi,T);
-    }
-    const double dVdT(const Eigen::VectorXd phi, const double T) {
-      return (-V(phi,T+2*h) + 8*V(phi,T+h) - 8*V(phi,T-h) + V(phi,T-2*h)) / (12.0 * h);  
-    }  
-    const double rho(const Eigen::VectorXd phi, const double T){
-      return V(phi,T) - 0.25*T*dVdT(phi,T);
-    }
-    const double alpha(const Eigen::VectorXd vacuum_1, const Eigen::VectorXd vacuum_2, double T){
-      return ( rho(vacuum_1,T) - rho(vacuum_2,T) ) / rho_R(T);
-    }
-    
-    /* Calcualte alpha along the phase */
-    Eigen::VectorXd vacuum_of_phase_at_T(Phase phase1, double T){
-      return tf.pf.phase_at_T(phase1, T).x;
-    }
-    const double V(Phase phase1, const double T) {
-      return tf.pf.P.V(vacuum_of_phase_at_T(phase1,T),T);
-    }
-    const double dVdT(Phase phase1, const double T) {
-      return (-V(phase1,T+2*h) + 8*V(phase1,T+h) - 8*V(phase1,T-h) + V(phase1,T-2*h)) / (12.0 * h);  
-    }  
-    const double rho(Phase phase1, const double T){
-      return V(phase1,T) - 0.25*T*dVdT(phase1,T);
-    }
-    const double alpha(Phase phase1, Phase phase2, double T){
-      return ( rho(phase1,T) - rho(phase1,T) ) / rho_R(T);
-    }
-
-    /* Using least squares method to fit a quadratic polynomial for solving the derivative of the action functional. */
-    Eigen::Vector3d quadraticFit(Eigen::VectorXd& x, Eigen::VectorXd& y) {  
-      int n = x.size();  
-      Eigen::Matrix3d A;  
-      Eigen::Vector3d b;  
-      A << n, x.sum(), (x.array() * x.array()).sum(),  
-         x.sum(), (x.array() * x.array()).sum(), (x.array() * x.array() * x.array()).sum(),  
-         (x.array() * x.array()).sum(), (x.array() * x.array() * x.array()).sum(), (x.array() * x.array() * x.array() * x.array()).sum();  
-      b << y.sum(),  
-         (x.array() * y.array()).sum(),  
-         (x.array() * x.array() * y.array()).sum();  
-      // slove A * coeff = b  
-      Eigen::Vector3d coeff = A.colPivHouseholderQr().solve(b);  
-      return coeff;  
-    }
-  
-    const double S3T(Phase phase1, Phase phase2, double T, size_t i_unique){
-      return tf.get_action(phase1, phase2, T, i_unique)/T;
-    }
-    
-    const double dSdT(Phase phase1, Phase phase2, double T, size_t i_unique){
-      Eigen::VectorXd x,y;
-      for (int ii=0; ii <= np_dSdT; ii++ ){
-        double Ti = T - h + ii * 2. * h / np_dSdT;
-        double S = S3T(phase1,phase2,T,i_unique);
-        if ( (not std::isnan(S)) and (not std::isnan(S)) ) { 
-          x << Ti;
-          y << S3T(phase1,phase2,T,i_unique);
-        }
-      }
-      // TODO add check on num of valid x
-      Eigen::Vector3d coeff = quadraticFit(x, y); // coeff[0] x^2 + coeff[1] x + coeff[2]
-      return 2 * coeff[0] * T + coeff[1];
-    }
-    
-    const double beta(Phase phase1, Phase phase2, double T, size_t i_unique){
-      return T * H(T) * dSdT(phase1, phase2, T, i_unique);
-    }
   
 public:
   explicit GravWaveCalculator(TransitionFinder tf_) :
   tf(tf_) {
-    std::vector<Transition> trans = tf.get_transitions();
-    
-    for (const auto& ti : trans){
-      double TN = ti.TN;
-      if (std::isnan(TN))
-      {
-        std::cout<<  "Nucleation temperature dose not exist. GW will not be calculated !";
-      } else {
-        std::vector<Eigen::VectorXd>  vacua = tf.get_vacua_at_T(ti.true_phase,ti.false_phase,TN,ti.key);
-
-        transitions_params.push_back({alpha(vacua[0],vacua[1],TN), beta(ti.true_phase,ti.false_phase,TN,ti.key), ti.TN});     
-      }
+    std::vector<Transition> trans_ = tf.get_transitions();
+    for (const auto& ti : trans_){
+      if (std::isnan(ti.TN))
+        LOG(debug) <<  "Nucleation temperature dose not exist. GW will not be calculated !";
+      else
+        trans.push_back(ti);
     }
   }
   virtual ~GravWaveCalculator() = default;
     
-    void Set_frequency_list(double begin_log_frequency = -4, double end_log_frequency = 1, double num_frequency = 500);
-    void Print_parameter();
-    void Write_to_csv(const std::tuple<double, double, std::vector<double>, std::vector<double>>& data, const std::string& filename);
-    double Kappa_sound_wave(double alpha);
-    double GW_bubble_collision(double f, double alpha, double beta_H, double T_ref);
-    double GW_sound_wave(double f, double alpha, double beta_H, double T_ref);
-    double GW_turbulence(double f, double alpha, double beta_H, double T_ref);
-    void GW_total_spectrum();
+  /** Pretty-printer for set of transitions in this object */
+  friend std::ostream& operator << (std::ostream& o, const GravWaveCalculator& a);
 
+  /** Functions to calculate amplitude at a fixed frequency*/
+  double Kappa_sound_wave(double alpha);
+  double GW_bubble_collision(double f, double alpha, double beta_H, double T_ref);
+  double GW_sound_wave(double f, double alpha, double beta_H, double T_ref);
+  double GW_turbulence(double f, double alpha, double beta_H, double T_ref);
+
+  /** Calculate GW spectrum for one transition*/
+  GravWaveSpectrum cal_spectrum(double alpha, double beta, double Tref);
+  /** Calculate GW spectrums for all the transitions*/
+  std::vector<GravWaveSpectrum> cal_spectrums();
+  /** Return GW spectrums for all the transitions*/
+  std::vector<GravWaveSpectrum> get_spectrums() {return spectrums;}
+  
+  /** Sum GW spectrums*/
+  GravWaveSpectrum sum_spectrums(std::vector<GravWaveSpectrum> sps);
+  /** Return the summed GW spectrum*/
+  GravWaveSpectrum get_total_spectrum() {return total_spectrum;}
+  
+  /** Write a GW spectrum to a text file*/
+  void write_spectrum_to_text(GravWaveSpectrum sp, const std::string &filename);
+
+  /* Calcualte alpha with fixed phi */
+  const double get_alpha(const Eigen::VectorXd vacuum_1, const Eigen::VectorXd vacuum_2, double T);
+  /* Calcualte alpha along the phases */
+  const double get_alpha(Phase phase1, Phase phase2, double T);
+  
+  /* Calcualte beta */
+  const double get_beta(Phase phase1, Phase phase2, double T, size_t i_unique);
+  
+private:
+  
+  TransitionFinder tf;
+  
+  /** Degree of freedom */
+  PROPERTY(double, dof, 106.75)
+  /** Velocity of the bubble wall */
+  PROPERTY(double, vw, 0.3)
+  /** ?? */
+  PROPERTY(double, epsilon, 0.1)
+  /** Gravitational constant */
+  const double G = 6.7088e-39;
+  
+  /** GW spectrums for all the transitions*/
+  std::vector<GravWaveSpectrum> spectrums;
+  /** Summed GW spectrum*/
+  GravWaveSpectrum total_spectrum;
+  /** All transitions with valid TN*/
+  std::vector<Transition> trans;
+  
+  /** Lower bound on the frequency of the GW spectrum*/
+  PROPERTY(double, min_frequency, 1e-4)
+  /** Upper bound on the frequency of the GW spectrum*/
+  PROPERTY(double, max_frequency, 1e1)
+  /** Number of points for  the frequency of the GW spectrum*/
+  PROPERTY(int, num_frequency, 500)
+  /** Temperature threshold for using bubble collision*/
+  PROPERTY(double, T_threshold_bubble_collision, 10)
+  
+  /** The step-size in numerical derivative for dVdT*/
+  PROPERTY(double, h_dVdT, 1e-2)
+  /** The step-size in numerical derivative for dSdT */
+  PROPERTY(double, h_dSdT, 1e-1)
+  /** The number of points used in numerical derivative of action */
+  PROPERTY(double, np_dSdT, 5)
+
+    /*  */
+  const double rho_R(double T);
+
+  /* Hubble constant */
+  const double H(double T);
+
+  /* Functions for calcualting alpha with fixed phi */
+  const double V(const Eigen::VectorXd phi, const double T);
+  const double dVdT(const Eigen::VectorXd phi, const double T);
+  const double rho(const Eigen::VectorXd phi, const double T);
+  
+  /* Functions for calcualting alpha along the phase */
+  Eigen::VectorXd vacuum_of_phase_at_T(Phase phase1, double T);
+  const double V(Phase phase1, const double T);
+  const double dVdT(Phase phase1, const double T);
+  const double rho(Phase phase1, const double T);
+
+  /* Using linear regression for solving the derivative of the action functional. */
+  Eigen::Vector2d LinearRegression(std::vector<double> x_, std::vector<double> y_);
+  /* Get action over temperature */
+  const double S3T(Phase phase1, Phase phase2, double T, size_t i_unique);
+  /* Calculate derivative of S3/T to T */
+  const double dSdT(Phase phase1, Phase phase2, double T, size_t i_unique);
+  
 };
 
 }  // namespace PhaseTracer
