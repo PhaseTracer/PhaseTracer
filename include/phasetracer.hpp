3595ebdbaaea345178c78ef7d303efb63c21a43e
phyzhangyang <yang.phy@foxmail.com>
2024-11-06 21:02:57 +0800

    add phasetracer.hpp

---
+++ include/phasetracer.hpp
@@ -0,0 +1,33 @@
+// ====================================================================
+// This file is part of PhaseTracer
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// ====================================================================
+
+#ifndef PHASETRACER_HPP_INCLUDED
+#define PHASETRACER_HPP_INCLUDED
+
+#include "logger.hpp"
+#include "phase_finder.hpp"
+#include "transition_finder.hpp"
+#include "action_calculator.hpp"
+#include "gravwave_calculator.hpp"
+/* if you wish to make plots,
+   include relevant plotting files */
+#include "phase_plotter.hpp"
+#include "potential_plotter.hpp"
+#include "potential_line_plotter.hpp"
+#include "spectrum_plotter.hpp"
+
+#endif
