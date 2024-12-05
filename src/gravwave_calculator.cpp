ef7dc9af1b11bf57e24f8dc670a64fd7fd152ec6
Yang Zhang <yang.phy@foxmail.com>
2024-12-05 14:44:47 +0800

    When Tref is too low, the calculation of the gravitational wave spectrum is disabled.

--- src/gravwave_calculator.cpp
+++ src/gravwave_calculator.cpp
@@ -256,6 +256,9 @@ GravWaveSpectrum GravWaveCalculator::sum_spectrums(const std::vector<GravWaveSpe
 std::vector<GravWaveSpectrum> GravWaveCalculator::calc_spectrums() {
   for (const auto &ti : trans) {
     double Tref = ti.TN;
+    if (Tref < 1.5*h_dSdT) {
+      continue;
+    }
     std::vector<Eigen::VectorXd> vacua = tf.get_vacua_at_T(ti.true_phase, ti.false_phase, Tref, ti.key);
     double alpha = get_alpha(vacua[0], vacua[1], Tref);
     double beta_H = get_beta_H(ti.true_phase, ti.false_phase, Tref, ti.key);
