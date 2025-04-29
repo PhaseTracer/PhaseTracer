7edb4aa275c26ab9b61b66006c7efab0052e2644
phyzhangyang <yang.phy@foxmail.com>
2025-04-29 15:50:35 +0800

    align the output phi values in the profile with the corresponding points on the tunneling_path“

--- example/run_2D_test_model.cpp
+++ example/run_2D_test_model.cpp
@@ -48,19 +48,13 @@ int main(int argc, char *argv[]) {
     std::cout << "action = " << std::setprecision(15) << action << std::endl;
 
     std::ofstream file1("profile1D_for_2d_example.txt");
-    file1 << "R,Phi,dPhi\n";
     PhaseTracer::Profile1D profile = ac.get_bubble_profile();
+    std::vector<Eigen::VectorXd> phi_for_profile = ac.get_phi_for_profile();
+    file1 << "R,Phi,dPhi,phi_s1,phi_s2\n";
     for (int i = 0; i < profile.R.size(); ++i) {
-      file1 << std::setprecision(10) << profile.R[i] << "," << profile.Phi[i] << "," << profile.dPhi[i] << std::endl;
+      file1 << std::setprecision(10) << profile.R[i] << "," << profile.Phi[i] << "," << profile.dPhi[i] << "," << phi_for_profile[i][0] << "," << phi_for_profile[i][1] << std::endl;
     }
     file1.close();
-
-    std::ofstream file2("path_for_2d_example.txt");
-    std::vector<Eigen::VectorXd> path = ac.get_tunneling_path();
-    for (int i = 0; i < path.size(); ++i) {
-      file2 << std::setprecision(10) << path[i][0] << "\t" << path[i][1] << std::endl;
-    }
-    file2.close();
 
     PhaseTracer::potential_plotter(model, tf.get_transitions().front().TC, "2D_test_model", 0., 2., 0.01, -2., 0., 0.01);
     PhaseTracer::potential_line_plotter(model, tf.get_transitions(), "2D_test_model");
