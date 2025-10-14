629c68018ea0df87f6b07c85782d4a8ca106150c
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-10-14 14:18:57 +0800

    fix headers

--- EffectivePotential/src/potential.cpp
+++ EffectivePotential/src/potential.cpp
@@ -15,6 +15,8 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // ====================================================================
 
+#include <boost/numeric/odeint.hpp>
+
 #include "potential.hpp"
 #include "pow.hpp"
 
