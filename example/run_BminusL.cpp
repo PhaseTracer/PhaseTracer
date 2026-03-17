0f0d5d04c17148a04be4e15fd42c84f6ec255e29
Peter Athron <peter.athron@coepp.org.au>
2026-03-17 13:00:43 +0800

    fix indentation

--- example/run_BminusL.cpp
+++ example/run_BminusL.cpp
@@ -106,155 +106,155 @@ int main(int argc, char *argv[]) {
 
   // Construct model
   EffectivePotential::BminusL model(inputFileName);
- ///Very likely connected to the problem!
- std::ifstream inputFile(inputFileName);
+  ///Very likely connected to the problem!
+  std::ifstream inputFile(inputFileName);
 
- if (!inputFile)
-   {
-     std::cerr << "Cannot open the file: " << inputFileName << std::endl;
-     return -1;
-   }
+  if (!inputFile)
+    {
+      std::cerr << "Cannot open the file: " << inputFileName << std::endl;
+      return -1;
+    }
  
- std::string line;
- std::vector<double> data;
- int dataValues = 4;
+  std::string line;
+  std::vector<double> data;
+  int dataValues = 4;
  
- std::getline(inputFile, line);
- if (line.size() == 0)
-   {
-     std::cerr << "Parameter point line is empty!" << std::endl;
-     return -1;
-   }
-
- // Keep only the first 4 values, in case there is other data stored in the file.
- data = model.split(line, ' ', dataValues);
- std::cout << "passed vev = " << data[1] << std::endl; 
- double mass_scale = data[1];
- double small_scale = mass_scale*pow(10, -3);
- inputFile.close();
-
-
- ///Very likely connected to the problem!
- model.set_h(0.001 * small_scale);
-       //model.set_h(0.001*pow(10,7));
- //model.set_h(0.001);
- double Ttest = 1.5*pow(10,10);
- Eigen::VectorXd phitest(1);
- phitest(0) = pow(10,10);
- Eigen::VectorXd phizero(1);
- phizero(0) = 0.0;
- std::cout << "model.V(phitest, Ttest) = " << model.V(phitest, Ttest) << std::endl;
- std::cout << "model.V(phitest, 0) = " << model.V(phitest, 0) << std::endl;
- std::cout << "model.V(0, Ttest) = " << model.V(phizero, Ttest) << std::endl;
- 
- std::cout << "model.V(0, 0) = " << model.V(phizero, 0) << std::endl;
- 
- std::cout << " model.V(phitest, Ttest) -  model.V(0, 0) = "  <<  model.V(phitest, Ttest) -  model.V(phizero, 0) << std::endl; 
- std::cout << " should equal zero = " << std::pow(0,0.5) << std::endl;
- std::cout << " should equal zero = " << std::pow(0.0,0.5) << std::endl;
- 
- std::cout << " should also equal zero = " << std::pow(phizero(0),0.5) << std::endl;
- std::cout <<  " JB(0) = "  << EffectivePotential::J_B(0) << std::endl;
+  std::getline(inputFile, line);
+  if (line.size() == 0)
+    {
+      std::cerr << "Parameter point line is empty!" << std::endl;
+      return -1;
+    }
+  
+  // Keep only the first 4 values, in case there is other data stored in the file.
+  data = model.split(line, ' ', dataValues);
+  std::cout << "passed vev = " << data[1] << std::endl; 
+  double mass_scale = data[1];
+  double small_scale = mass_scale*pow(10, -3);
+  inputFile.close();
 
 
+  ///Very likely connected to the problem!
+  model.set_h(0.001 * small_scale);
+  //model.set_h(0.001*pow(10,7));
+  //model.set_h(0.001);
+  double Ttest = 1.5*pow(10,10);
+  Eigen::VectorXd phitest(1);
+  phitest(0) = pow(10,10);
+  Eigen::VectorXd phizero(1);
+  phizero(0) = 0.0;
+  std::cout << "model.V(phitest, Ttest) = " << model.V(phitest, Ttest) << std::endl;
+  std::cout << "model.V(phitest, 0) = " << model.V(phitest, 0) << std::endl;
+  std::cout << "model.V(0, Ttest) = " << model.V(phizero, Ttest) << std::endl;
  
- double stepsize = 0.001*small_scale;
- // double stepsize = 0.001*pow(10,7);
- std::cout << "test stepsize = " <<  stepsize << std::endl;
- //Ttest = 15000000000;
- Ttest=0;
- //phitest(0) =  1354.0946118877914;
- //phitest(0) = -919.21635596052397;
- phitest(0) = 0.6782 * mass_scale;
- Eigen::VectorXd phitestplus = phitest;
- Eigen::VectorXd phitestminus = phitest;
- phitestplus(0) += stepsize;
- phitestminus(0) -= stepsize;
- std::cout << "  model.V(phitest=-919.1...., 0) = "  << model.V(phitest, 0)<< std::endl;
- std::cout << "  model.V(phitestplus=" <<phitestplus << ",  0) = "  << model.V(phitestplus, 0)<< std::endl;
- std::cout << "  model.V(phitestminus=" <<phitestminus << ",  0) = "  << model.V(phitestminus, 0)<< std::endl;
- double deriv_2nd_order = (model.V(phitestplus, Ttest) - model.V(phitestminus, Ttest)) / (2*stepsize);
- double double_deriv =   (model.V(phitestplus, Ttest) + model.V(phitestminus, Ttest) - 2 * model.V(phitest, Ttest)) / (stepsize*stepsize);
- std::cout << "deriv_2nd_order = "  << deriv_2nd_order << std::endl;
- std::cout << "double_deriv = "     << double_deriv    << std::endl;
+  std::cout << "model.V(0, 0) = " << model.V(phizero, 0) << std::endl;
  
+  std::cout << " model.V(phitest, Ttest) -  model.V(0, 0) = "  <<  model.V(phitest, Ttest) -  model.V(phizero, 0) << std::endl; 
+  std::cout << " should equal zero = " << std::pow(0,0.5) << std::endl;
+  std::cout << " should equal zero = " << std::pow(0.0,0.5) << std::endl;
  
- 
- PhaseTracer::PhaseFinder pf(model);
- pf.set_t_high(1.5 * mass_scale); //Essential
- 
- pf.set_v(mass_scale);
+  std::cout << " should also equal zero = " << std::pow(phizero(0),0.5) << std::endl;
+  std::cout <<  " JB(0) = "  << EffectivePotential::J_B(0) << std::endl;
+
 
  
- pf.set_upper_bounds({10 * mass_scale}); //Essential
- pf.set_lower_bounds({-10 * mass_scale}); //Essential
+  double stepsize = 0.001*small_scale;
+  // double stepsize = 0.001*pow(10,7);
+  std::cout << "test stepsize = " <<  stepsize << std::endl;
+  //Ttest = 15000000000;
+  Ttest=0;
+  //phitest(0) =  1354.0946118877914;
+  //phitest(0) = -919.21635596052397;
+  phitest(0) = 0.6782 * mass_scale;
+  Eigen::VectorXd phitestplus = phitest;
+  Eigen::VectorXd phitestminus = phitest;
+  phitestplus(0) += stepsize;
+  phitestminus(0) -= stepsize;
+  std::cout << "  model.V(phitest=-919.1...., 0) = "  << model.V(phitest, 0)<< std::endl;
+  std::cout << "  model.V(phitestplus=" <<phitestplus << ",  0) = "  << model.V(phitestplus, 0)<< std::endl;
+  std::cout << "  model.V(phitestminus=" <<phitestminus << ",  0) = "  << model.V(phitestminus, 0)<< std::endl;
+  double deriv_2nd_order = (model.V(phitestplus, Ttest) - model.V(phitestminus, Ttest)) / (2*stepsize);
+  double double_deriv =   (model.V(phitestplus, Ttest) + model.V(phitestminus, Ttest) - 2 * model.V(phitest, Ttest)) / (stepsize*stepsize);
+  std::cout << "deriv_2nd_order = "  << deriv_2nd_order << std::endl;
+  std::cout << "double_deriv = "     << double_deriv    << std::endl;
  
- std::cout << "pf.get_upper_bounds() = " << *(pf.get_upper_bounds().begin()) << std::endl;
- std::cout << "pf.get_lower_bounds() = " << *(pf.get_lower_bounds().begin()) << std::endl;
  
- /// seems like it should be ok as we have relative tolerance overwealming it when small
- // unless we compare to phases which are very close maybe...
- pf.set_x_abs_identical(1 * small_scale); // Essential to scale
- pf.set_seed(10.23);
- pf.set_x_abs_jump(0.5*small_scale);
- // passed to nlopt:opt
- pf.set_find_min_x_tol_abs(0.0001*small_scale);
+ 
+  PhaseTracer::PhaseFinder pf(model);
+  pf.set_t_high(1.5 * mass_scale); //Essential
+ 
+  pf.set_v(mass_scale);
+
+ 
+  pf.set_upper_bounds({10 * mass_scale}); //Essential
+  pf.set_lower_bounds({-10 * mass_scale}); //Essential
+ 
+  std::cout << "pf.get_upper_bounds() = " << *(pf.get_upper_bounds().begin()) << std::endl;
+  std::cout << "pf.get_lower_bounds() = " << *(pf.get_lower_bounds().begin()) << std::endl;
+ 
+  /// seems like it should be ok as we have relative tolerance overwealming it when small
+  // unless we compare to phases which are very close maybe...
+  pf.set_x_abs_identical(1 * small_scale); // Essential to scale
+  pf.set_seed(10.23);
+  pf.set_x_abs_jump(0.5*small_scale);
+  // passed to nlopt:opt
+  pf.set_find_min_x_tol_abs(0.0001*small_scale);
        
- pf.set_find_min_min_step(1e-4);  //scaling doesn't affect the time either way
+  pf.set_find_min_min_step(1e-4);  //scaling doesn't affect the time either way
 
- /// initial mimum step step size for tracing
- pf.set_find_min_trace_abs_step(1. * small_scale);
+  /// initial mimum step step size for tracing
+  pf.set_find_min_trace_abs_step(1. * small_scale);
  
- /// initial minimum step step size for locating,
- pf.set_find_min_locate_abs_step(1. * small_scale); // Essential
+  /// initial minimum step step size for locating,
+  pf.set_find_min_locate_abs_step(1. * small_scale); // Essential
  
- /// set to the smaller of time_scale*dt_max_rel and dt_max_abs 
- //  so if this stays fixed as scale increases can be a problem,
- // dt steps get smaller and smaller relative to T
- pf.set_dt_max_abs(50 * small_scale); //Essential
+  /// set to the smaller of time_scale*dt_max_rel and dt_max_abs 
+  //  so if this stays fixed as scale increases can be a problem,
+  // dt steps get smaller and smaller relative to T
+  pf.set_dt_max_abs(50 * small_scale); //Essential
  
- // set to maximum of dt_min_rel * time_scale and dt_min_abs so as scale increases dt_min_rel * time_scale dominates and abs is irrelevant
- pf.set_dt_min_abs(1.e-10 * small_scale);//
+  // set to maximum of dt_min_rel * time_scale and dt_min_abs so as scale increases dt_min_rel * time_scale dominates and abs is irrelevant
+  pf.set_dt_min_abs(1.e-10 * small_scale);//
  
   /** Tolerance for checking whether Hessian was singular */
- //pf.hessian_singular_rel_tol(1.e-2);
+  //pf.hessian_singular_rel_tol(1.e-2);
   /** Tolerance for checking solutions to linear algebra */
- //pf.linear_algebra_rel_tol(1.e-3);  
+  //pf.linear_algebra_rel_tol(1.e-3);  
  
- pf.set_check_vacuum_at_high(true);
- pf.set_check_hessian_singular(true);
- pf.set_check_dx_min_dt(true);
- pf.find_phases();
+  pf.set_check_vacuum_at_high(true);
+  pf.set_check_hessian_singular(true);
+  pf.set_check_dx_min_dt(true);
+  pf.find_phases();
 
- // Make TransitionFinder object and find the transitions
- PhaseTracer::TransitionFinder tf(pf);
- tf.set_check_subcritical_transitions(check_subcrit);
- tf.set_assume_only_one_transition(!allow_phase_oscillation);
- tf.find_transitions();
- tf.find_transition_paths(false);
+  // Make TransitionFinder object and find the transitions
+  PhaseTracer::TransitionFinder tf(pf);
+  tf.set_check_subcritical_transitions(check_subcrit);
+  tf.set_assume_only_one_transition(!allow_phase_oscillation);
+  tf.find_transitions();
+  tf.find_transition_paths(false);
 
- std::cout << pf;
- std::cout << tf;
+  std::cout << pf;
+  std::cout << tf;
 
- if (!bNoTransitionPathFinding)
- {
-   LOG(debug) << pf;
-   LOG(debug) << "Finding transition paths...";
-   tf.find_transition_paths(true);
- }
+  if (!bNoTransitionPathFinding)
+    {
+      LOG(debug) << pf;
+      LOG(debug) << "Finding transition paths...";
+      tf.find_transition_paths(true);
+    }
 
- if (bDebug)
- {
-   std::cout << pf;
-   std::cout << tf;
+  if (bDebug)
+    {
+      std::cout << pf;
+      std::cout << tf;
+      
+      if (!bNoTransitionPathFinding)
+	{
+	  printPaths(tf.get_transition_paths());
+	}
+    }
 
-   if (!bNoTransitionPathFinding)
-   {
-     printPaths(tf.get_transition_paths());
-   }
- }
+  PhaseTracer::phase_plotter(tf, outputFolderName, "phase_structure", bPlot);
 
- PhaseTracer::phase_plotter(tf, outputFolderName, "phase_structure", bPlot);
-
- return 0;
+  return 0;
 }
